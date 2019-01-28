7d9d3a722cafae03591592af628c5cb455af75d6
Dionny Santiago <dionny_santiago@ultimatesoftware.com>
2019-01-27 22:43:56 -0500

    Relying on a base python Docker image on DockerHub (AISTA repo).

--- build_base_images.sh
+++ build_base_images.sh
@@ -4,6 +4,10 @@ set -e # fail fast
 
 here=`pwd`
 
+# We push the image below to DockerHub as it takes a while to build.
+# Keep this commented out unless there is a need to push a new image to DockerHub.
+# docker build -f deployment/aist-python-base/Dockerfile -t aista-python-base .
+
 docker build -f deployment/aist-python/Dockerfile -t aist-python .
 
 docker build -f deployment/aist-tensorflow/Dockerfile -t aist-tensorflow .
