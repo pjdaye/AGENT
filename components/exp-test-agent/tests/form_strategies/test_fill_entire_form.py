0cf178cc87cbe46a748069677d1ae24129ba3224
David Adamo <David_Adamo@ultimatesoftware.com>
2019-01-31 13:01:55 -0500

    form filler unit tests

---
+++ components/exp-test-agent/tests/form_strategies/test_fill_entire_form.py
@@ -0,0 +1,130 @@
+from unittest.mock import Mock, patch
+import pytest
+from Clients.runner_client import RunnerClient
+from abstraction.actionable_state import ActionableState
+from form_strategies.fill_entire_form import FillEntireForm
+
+
+@pytest.fixture
+def runner():
+    runner_mock = Mock(spec=RunnerClient)
+    runner_mock.perform_action.return_value = True
+    return runner_mock
+
+
+@pytest.fixture
+def form_expert():
+    form_expert_mock = Mock()
+    form_expert_mock.get_concrete_value.return_value = 'Guido'
+    return form_expert_mock
+
+
+def test_execute_form_fill_with_no_actionable_widgets_returns_true(runner, form_expert):
+    # Arrange
+    actionable_state = ActionableState()
+    form_fill_strategy = FillEntireForm(form_expert)
+
+    # Act
+    result = form_fill_strategy.execute(runner, actionable_state)
+
+    # Assert
+    assert result is True
+
+
+def test_execute_form_fill_with_one_settable_widget_returns_true(runner, form_expert):
+    # Arrange
+    actionable_state = ActionableState()
+    widget = {
+        'actions': ['set'],
+        'label': 'widget_label',
+        'key': 'widget_key',
+        'selector': 'widget_selector'
+    }
+    actionable_state.add_widget(widget)
+    form_fill_strategy = FillEntireForm(form_expert)
+
+    # Act
+    result = form_fill_strategy.execute(runner, actionable_state)
+
+    # Assert
+    assert result is True
+
+
+def test_execute_form_fill_with_one_settable_widget_calls_runner_perform_action_once(runner, form_expert):
+    # Arrange
+    actionable_state = ActionableState()
+    widget = {
+        'actions': ['set'],
+        'label': 'widget_label',
+        'key': 'widget_key',
+        'selector': 'widget_selector'
+    }
+    actionable_state.add_widget(widget)
+    form_fill_strategy = FillEntireForm(form_expert)
+
+    # Act
+    form_fill_strategy.execute(runner, actionable_state)
+
+    # Assert
+    runner.perform_action.assert_called_once()
+
+
+def test_execute_form_fill_with_two_settable_widgets_calls_runner_perform_action_twice(runner, form_expert):
+    # Arrange
+    actionable_state = ActionableState()
+    widget = {
+        'actions': ['set'],
+        'label': 'widget_label',
+        'key': 'widget_key',
+        'selector': 'widget_selector'
+    }
+    actionable_state.add_widget(widget)
+    actionable_state.add_widget(widget)
+    form_fill_strategy = FillEntireForm(form_expert)
+
+    # Act
+    form_fill_strategy.execute(runner, actionable_state)
+
+    # Assert
+    assert runner.perform_action.call_count == 2
+
+
+def test_execute_form_fill_with_one_non_settable_widget_does_not_perform_action(runner, form_expert):
+    # Arrange
+    actionable_state = ActionableState()
+    widget = {
+        'actions': [],
+        'label': 'widget_label',
+        'key': 'widget_key',
+        'selector': 'widget_selector'
+    }
+    actionable_state.add_widget(widget)
+    form_fill_strategy = FillEntireForm(form_expert)
+
+    # Act
+    form_fill_strategy.execute(runner, actionable_state)
+
+    # Assert
+    runner.perform_action.assert_not_called()
+
+@patch('form_strategies.fill_entire_form.LOGGER')
+def test_execute_form_fill_returns_false_when_perform_action_fails(_, runner, form_expert):
+    # Arrange
+    actionable_state = ActionableState()
+    widget = {
+        'actions': ['set'],
+        'label': 'widget_label',
+        'key': 'widget_key',
+        'selector': 'widget_selector'
+    }
+    actionable_state.add_widget(widget)
+    runner.perform_action.return_value = False
+    form_fill_strategy = FillEntireForm(form_expert)
+
+    # Act
+    result = form_fill_strategy.execute(runner, actionable_state)
+
+    # Assert
+    assert result is False
+
+
